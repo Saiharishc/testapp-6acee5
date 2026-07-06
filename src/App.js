42628c659a98891063241ebbf4ef69ee5f553c72
Saiharishc <51015572+Saiharishc@users.noreply.github.com>
2026-07-06 06:16:32 -0500

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to TestApp</h1>
+        <p>This is the frontend for your FastAPI backend.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
